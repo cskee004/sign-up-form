609d015223a6a05f72ac9d74a0dbdd9da74a2749
Chris Skeens <cskee004@odu.edu>
2024-07-17 15:56:18 -0400

    Commit

---
+++ form.js
@@ -0,0 +1,27 @@
+
+
+var password = document.getElementById("pwd")
+, confirmPassword = document.getElementById("cpwd");
+
+password.value = "abc!1";
+confirmPassword.value = "abc!1";
+
+function validatePassword() {
+    if (password.value != confirmPassword.value) {
+        confirmPassword.setCustomValidity("Passwords Don't Match")
+        console.log("passwords dont match");
+    }
+    else {
+        confirmPassword.setCustomValidity('');
+        console.log("passwords match")
+    }
+}
+
+validatePassword();
+/* 
+password.onchange = validatePassword;
+confirmPassword.onkeyup = validatePassword;
+*/
+
+var fName = document.getElementById("fname")
+, lName = document.getElementById("lname");
